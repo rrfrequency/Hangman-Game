ddaaa9049d951362a98b1017e1143e8e58b03591
Ryan Retirado <ryanretirado@Maes-MBP.fios-router.home>
2017-12-12 02:28:49 -0500

    JS improvement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,31 +8,48 @@ const word = ["Stark", "Lannister", "Tully", "Greyjoy", "Martell", "Tyrell", "Ba
 // Randomize word choice
 let	randNum = Math.floor(Math.random() * word.length);
 let choosenWord = word[randNum];
-let underScores = [];
-console.log(choosenWord);
+let underScore = [];
+
+//DOM manipulation
+let docUnderScore = document.getElementsByClassName("game-section")
+let docRightGuess = document.getElementsByClassName("letters-buttons")
+let docWrongGuess = document.getElementsByClassName("wrong-letters-chosen")
 
 
 // Create underscores based on length of words
 let generateUnderscore = () => {
 	for(let i = 0; i < choosenWord.length; i++) {
-		underScores.push("_")
+		underScore.push("_")
 	}
-	return underScores
+	return underScore
 }
-
-console.log(generateUnderscore());
 
 // Create an event listener for user letter guess
 document .addEventListener("keypress", (event) => {
-	let keycode = event.keyCode;
-	console.log(event)
-});
+	let keyword = String.fromCharCode(event.keyCode);
 
 // Check if that guess is correct
+	if(choosenWord.indexOf(keyword) > -1) {
 
 // If correct push to correct word array
+		rightword.push(keyword);
 
+//replace underscore with right letter
+		underScore[choosenWord.indexof(keyword)] = keyword;
+		
+//Checks to see if user word matches guesses
+		if(underScore.join(" ") == choosenWord) {
+			alert("You win");
+
+		}
+	}
+	else {
+		wrongWord.push(keyword);
+		
+	}
+});
 // If incorrect push to incorrect word array
 
 // If too many guesses, display game over alert
+
 
